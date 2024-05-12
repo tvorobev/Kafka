982a607981593f3c007593624a1b98d9d992a05d
Bоробьев Трофим <112179972+tvorobev@users.noreply.github.com>
2024-05-12 16:02:46 +0300

    1

---
+++ project_without_sasl/producer_without_sasl.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Producer
+
+config = {
+    'bootstrap.servers': 'localhost:29092',  # адрес Kafka сервера
+    'client.id': 'simple-producer'
+}
+
+producer = Producer(**config)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+
+def send_message(data):
+    try:
+        # Асинхронная отправка сообщения
+        producer.produce('test_topic', data.encode('utf-8'), callback=delivery_report)
+        producer.poll(0)  # Поллинг для обработки обратных вызовов
+    except BufferError:
+        print(f"Local producer queue is full ({len(producer)} messages awaiting delivery): try again")
+
+if __name__ == '__main__':
+    send_message('Hello, World!')
+    producer.flush()
