c71138ba1c88bdbca3dee080a6275e899fb7c6f5
tvorobev <112179972+tvorobev@users.noreply.github.com>
2024-06-13 00:53:20 +0300

    1
    1

---
+++ project_kafka_clickhouse/producer_json.py
@@ -0,0 +1,27 @@
+from confluent_kafka import Producer
+
+config = {
+    'bootstrap.servers': 'localhost:9092',  # адрес Kafka сервера
+    'client.id': 'simple-producer'
+}
+
+producer = Producer(**config)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+
+def send_message(data):
+    try:
+        # Асинхронная отправка сообщения
+        producer.produce('test_topic_json', data.encode('utf-8'), callback=delivery_report)
+        producer.poll(0)  # Поллинг для обработки обратных вызовов
+    except BufferError:
+        print(f"Local producer queue is full ({len(producer)} messages awaiting delivery): try again")
+
+if __name__ == '__main__':
+    send_message('{"shk_id": 456,'
+                 '"chrt_id": 789}')
+    producer.flush()
